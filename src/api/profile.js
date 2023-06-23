ec90308598d63ae774fc0ce1c85453b650f8fea8
mathysPaul <60519550+mathysPaul@users.noreply.github.com>
2023-06-23 22:20:54 +0900

    feat: profile page update & game added

--- src/api/profile.js
+++ src/api/profile.js
@@ -9,19 +9,28 @@ function fetchUserProfile() {
             document.getElementById('profile-picture').src = user.profile_image;
             document.getElementById('profile-banner').src = user.banner_image;
             document.querySelector('.profile-name').textContent = user.username;
+
+            updateBannerImage(user.banner_image);
+
+            const profileDescription = document.querySelector('.profile-description');
+            profileDescription.textContent = user.description;
+            // Store the initial value of the description
+            profileDescription.dataset.initialValue = user.description;
         });
 }
 
 function toggleEditMode() {
-    document.getElementById('profile-upload').style.display = 'block';
-    document.getElementById('banner-upload').style.display = 'block';
-    document.querySelector('.edit button').style.display = 'none';
-    document.querySelector('.edit button:nth-child(2)').style.display = 'block';
+    document.getElementById('profile-upload-label').style.visibility = 'visible';
+    document.getElementById('banner-upload-label').style.visibility = 'visible';
+    document.querySelector('.profile-description').contentEditable = 'true';
+    document.querySelector('.edit-profile').style.display = 'none';
+    document.querySelector('.save-profile').style.display = 'block';
 }
 
 function saveChanges() {
     const profileUpload = document.getElementById('profile-upload');
     const bannerUpload = document.getElementById('banner-upload');
+    const profileDescription = document.querySelector('.profile-description');
 
     const formData = new FormData();
     if (profileUpload.files.length > 0) {
@@ -29,10 +38,16 @@ function saveChanges() {
     }
     if (bannerUpload.files.length > 0) {
         formData.append('banner_image', bannerUpload.files[0]);
+        updateBannerImage(bannerUpload.files[0]);
+    }
+
+    // Check if the description has been modified
+    if (profileDescription.textContent !== profileDescription.dataset.initialValue) {
+        formData.append('description', profileDescription.textContent);
     }
 
     // Check if formData is not empty
-    if (formData.has('profile_image') || formData.has('banner_image')) {
+    if (formData.has('profile_image') || formData.has('banner_image') || formData.has('description')) {
         fetch('../backend/profile.php', {
             method: 'POST',
             body: formData
@@ -47,15 +62,17 @@ function saveChanges() {
             }
         });
     } else {
+        toggleViewMode();
         console.log('No data to update');
     }
 }
 
 function toggleViewMode() {
-    document.getElementById('profile-upload').style.display = 'none';
-    document.getElementById('banner-upload').style.display = 'none';
-    document.querySelector('.edit button').style.display = 'block';
-    document.querySelector('.edit button:nth-child(2)').style.display = 'none';
+    document.getElementById('profile-upload-label').style.visibility = 'hidden';
+    document.getElementById('banner-upload-label').style.visibility = 'hidden';
+    document.querySelector('.profile-description').contentEditable = 'false';
+    document.querySelector('.edit-profile').style.display = 'block';
+    document.querySelector('.save-profile').style.display = 'none';
 }
 
 function previewImage(event, elementId) {
@@ -65,4 +82,9 @@ function previewImage(event, elementId) {
         output.src = reader.result;
     };
     reader.readAsDataURL(event.target.files[0]);
+}
+
+function updateBannerImage(imageUrl) {
+    const profile = document.querySelector('.profile');
+    profile.style.setProperty('--banner-image', `url("${imageUrl}")`);
 }
---
+++ src/game/Game.js
@@ -0,0 +1,146 @@
+import Level from './Level.js';
+
+var arrowCodes = {
+    37: "left",
+    38: "up",
+    39: "right"
+};
+
+function trackKeys(codes) {
+    var touchstartX = 0;
+    var touchstartY = 0;
+    var touchendX = 0;
+    var touchendY = 0;
+
+    var pressed = Object.create(null);
+
+    function handler(event) {
+        console.log("key");
+        if (codes.hasOwnProperty(event.keyCode)) {
+            var down = event.type == "keydown";
+            pressed[codes[event.keyCode]] = down;
+            event.preventDefault();
+        }
+    }
+
+    addEventListener('touchstart', function(event) {
+        touchstartX =  event.touches[0].clientX;
+        touchstartY =  event.touches[0].clientY;
+        console.log("Start");
+        console.log( event.touches[0].clientX);
+
+        if (touchstartX > window.innerWidth / 2) {
+            console.log("RIGHT !!!!!!!!!!!!");
+            pressed["right"] = true;
+            pressed["left"] = false;
+        } else {
+            console.log("LEFT !!!!!!!!!!!!");
+            pressed["left"] = true;
+            pressed["right"] = false;
+        }
+
+    }, false);
+
+    addEventListener('touchend', function(event) {
+        touchendX =  event.changedTouches[0].clientX;
+        touchendY =  event.changedTouches[0].clientY;
+        console.log("END");
+        handleGesure();
+        pressed["left"] = false;
+        pressed["right"] = false;
+    }, false); 
+
+    function handleGesure() {
+        var swiped = 'swiped: ';
+        if (touchendX < touchstartX) {
+            console.log(swiped + 'left!');
+        }
+        if (touchendX > touchstartX) {
+            console.log(swiped + 'right!');
+        }
+        if (touchendY < touchstartY) {
+            console.log(swiped + 'down!');
+        }
+        if (touchendY > touchstartY) {
+            console.log(swiped + 'left!');
+        }
+        if (touchendY == touchstartY) {
+            console.log('tap!');
+        }
+    }
+
+
+    let lastTap = 0;
+
+        function detectDoubleTap(event) {
+            if (event.type == "touchend") {
+                pressed["up"] = false;
+                console.log("STOP--------------------------");
+            } else {
+                const curTime = new Date().getTime();
+                const tapLen = curTime - lastTap;
+                if (tapLen < 500 && tapLen > 0) {
+                  console.log('Double tapped!');
+                  pressed["up"] = true;
+                  event.preventDefault();
+                } else {
+                  pressed["up"] = false;
+                  console.log("-------------------------------------------");
+                }
+                lastTap = curTime;
+            }
+        };
+      
+
+    addEventListener("keydown", handler);
+    addEventListener("keyup", handler);
+    // addEventListener("doubletap", handlerUp);
+    addEventListener('touchend', detectDoubleTap, { passive: false });
+    addEventListener('touchstart', detectDoubleTap, { passive: false });
+    // addEventListener("touchstart", handlerTouch);
+    // addEventListener("touchstop", handlerTouch);
+    return pressed;
+}
+
+function runAnimation(frameFunc) {
+    var lastTime = null;
+
+    function frame(time) {
+        var stop = false;
+        if (lastTime != null) {
+            var timeStep = Math.min(time - lastTime, 100) / 1000;
+            stop = frameFunc(timeStep) === false;
+        }
+        lastTime = time;
+        if (!stop) requestAnimationFrame(frame);
+    }
+    requestAnimationFrame(frame);
+}
+
+var arrows = trackKeys(arrowCodes);
+
+function runLevel(level, Display, andThen) {
+    var display = new Display(document.querySelector(".game"), level);
+    runAnimation(function(step) {
+        level.animate(step, arrows);
+        display.drawFrame(step);
+        if (level.isFinished()) {
+            display.clear();
+            if (andThen) andThen(level.status);
+            return false;
+        }
+    });
+}
+
+function runGame(plans, Display) {
+    function startLevel(n) {
+        runLevel(new Level(plans[n]), Display, function(status) {
+            if (status == "lost") startLevel(n);
+            else if (n < plans.length - 1) startLevel(n + 1);
+            else alert("You win!");
+        });
+    }
+    startLevel(0);
+}
+
+export { runGame };
