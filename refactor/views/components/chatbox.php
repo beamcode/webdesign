b1ba8f22ef8720a6371fb7518ebb7376009421b3
mathysPaul <60519550+mathysPaul@users.noreply.github.com>
2023-07-18 02:40:17 +0900

    feat: design messages in chatbox

--- refactor/views/components/chatbox.php
+++ refactor/views/components/chatbox.php
@@ -1,42 +1,76 @@
 <div class="messages-area">
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
-    <div class="message"></div>
+    <div class="message">
+        <img class="profile-picture" src="https://i.pinimg.com/236x/bb/d4/4b/bbd44b37f18e40a01543b8b4721b1cce.jpg" alt="">
+        <div class="message-content">
+            <div class="message-header">
+                <span class="username">Didier</span>
+                <span class="date-time">18/07/2023 02:37</span>
+            </div>
+            <div class="message-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fermentum dui eu quam molestie mattis. Nulla in erat et dolor pharetra sit.
+            </div>
+        </div>
+    </div>
+    <div class="message">
+        <img class="profile-picture" src="https://i.pinimg.com/236x/bb/d4/4b/bbd44b37f18e40a01543b8b4721b1cce.jpg" alt="">
+        <div class="message-content">
+            <div class="message-header">
+                <span class="username">Didier</span>
+                <span class="date-time">18/07/2023 02:37</span>
+            </div>
+            <div class="message-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fermentum dui eu quam molestie mattis. Nulla in erat et dolor pharetra sit.
+            </div>
+        </div>
+    </div>
+    <div class="message">
+        <img class="profile-picture" src="https://i.pinimg.com/236x/bb/d4/4b/bbd44b37f18e40a01543b8b4721b1cce.jpg" alt="">
+        <div class="message-content">
+            <div class="message-header">
+                <span class="username">Didier</span>
+                <span class="date-time">18/07/2023 02:37</span>
+            </div>
+            <div class="message-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fermentum dui eu quam molestie mattis. Nulla in erat et dolor pharetra sit.
+            </div>
+        </div>
+    </div>
+    <div class="message">
+        <img class="profile-picture" src="https://i.pinimg.com/236x/bb/d4/4b/bbd44b37f18e40a01543b8b4721b1cce.jpg" alt="">
+        <div class="message-content">
+            <div class="message-header">
+                <span class="username">Didier</span>
+                <span class="date-time">18/07/2023 02:37</span>
+            </div>
+            <div class="message-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fermentum dui eu quam molestie mattis. Nulla in erat et dolor pharetra sit.
+            </div>
+        </div>
+    </div>
+    <div class="message">
+        <img class="profile-picture" src="https://i.pinimg.com/236x/bb/d4/4b/bbd44b37f18e40a01543b8b4721b1cce.jpg" alt="">
+        <div class="message-content">
+            <div class="message-header">
+                <span class="username">Didier</span>
+                <span class="date-time">18/07/2023 02:37</span>
+            </div>
+            <div class="message-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fermentum dui eu quam molestie mattis. Nulla in erat et dolor pharetra sit.
+            </div>
+        </div>
+    </div>
+    <div class="message">
+        <img class="profile-picture" src="https://i.pinimg.com/236x/bb/d4/4b/bbd44b37f18e40a01543b8b4721b1cce.jpg" alt="">
+        <div class="message-content">
+            <div class="message-header">
+                <span class="username">Didier</span>
+                <span class="date-time">18/07/2023 02:37</span>
+            </div>
+            <div class="message-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fermentum dui eu quam molestie mattis. Nulla in erat et dolor pharetra sit.
+            </div>
+        </div>
+    </div>
     <div class="message"></div>
     <div class="message"></div>
     <div class="message"></div>
