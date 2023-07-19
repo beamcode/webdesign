9cd1469499952b9d543a6fbdac47541d75a6bfae
Mac <mac@gvpn165034.sras.sic.shibaura-it.ac.jp>
2023-07-20 03:15:39 +0900

    t

--- views/home.php
+++ views/home.php
@@ -1,5 +1,5 @@
 <form style="width: 100%; margin: 10px 0 20px 0; display: flex; justify-content: center; align-items: center; gap: 10px;" method="POST" onsubmit="setYouTubeLink(event)">
-  <h1 style="font-size: 20px; white-space: nowrap;">Share Youtube Player</h1>
+  <h1 style="font-size: 20px; white-space: nowrap;">Shared Youtube Player</h1>
   <input name="youtubeLink" id="searchBar" autocomplete="off" required type="text" style="width: calc(100% - 70px); height: 40px; border-radius: 8px; border: 1px solid #ccc; padding: 5px;">
   <input name="message" id="idkanymore" autocomplete="off" type="text" style="display: none;" value="">
   <button id="playButton" type='text' placeholder="Paste a youtube link..." style="margin-left: 10px; height: 40px; border-radius: 8px; background-color: #4CAF50; color: white; border: none; padding: 0 20px;">Play</button>
@@ -20,6 +20,7 @@
         if (!response.ok) {
           throw new Error("Error setting YouTube link");
         }
+        sendMessage(event);
         return response.json();
       })
       .then(data => {
@@ -29,7 +30,6 @@
       .catch(error => {
         console.error('Error setting YouTube link:', error);
       });
-    sendMessage(event);
   }
 
   function checkYouTubeLink() {
