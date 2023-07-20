b20ea4d2582aecf7e515d5f14a2d6dedf3da5a15
mathysPaul <60519550+mathysPaul@users.noreply.github.com>
2023-07-20 09:18:36 +0900

    sam est une merde

--- views/home.php
+++ views/home.php
@@ -1,8 +1,29 @@
-<form style="width: 100%; margin: 10px 0 20px 0; display: flex; justify-content: center; align-items: center; gap: 10px;" method="POST" onsubmit="setYouTubeLink(event)">
-  <h1 style="font-size: 20px; white-space: nowrap;">Shared Youtube Player</h1>
-  <input name="youtubeLink" id="searchBar" autocomplete="off" required type="text" style="width: calc(100% - 70px); height: 40px; border-radius: 8px; border: 1px solid #ccc; padding: 5px;">
-  <input name="message" id="idkanymore" autocomplete="off" type="text" style="display: none;" value="">
-  <button id="playButton" type='text' placeholder="Paste a youtube link..." style="margin-left: 10px; height: 40px; border-radius: 8px; background-color: #4CAF50; color: white; border: none; padding: 0 20px;">Play</button>
+<form class="form-youtube" method="POST" onsubmit="setYouTubeLink(event)">
+  <!-- <input
+    style=""
+    > -->
+  <?php
+    Input(
+        // class: "youtube-link",
+        name: "youtubeLink",
+        id: "searchBar",
+        autoComplete: "off",
+        type: 'text',
+        required: true,
+        placeholder: "Paste a youtube link...",
+    );
+    Input(
+      class: "youtube-link-hidden",
+      name: "message",
+      id: "idkanymore",
+      autoComplete: "off",
+      type: 'text',
+    );
+    Button(
+      id: "playButton",
+      text: "Play",
+    );
+  ?>
 </form>
 <iframe id="videoFrame" width="100%" height="80%" style="border-radius: 8px;" src="" frameborder="0" allowfullscreen></iframe>
 
